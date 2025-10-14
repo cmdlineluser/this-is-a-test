3cf0bbdd55cc3c1752e269a43a642996a028ebd9
Karl Genockey <99486669+cmdlineluser@users.noreply.github.com>
2025-10-14 13:15:54 +0100

    Update list-order-by-oom.sql

--- list-order-by-oom.sql
+++ list-order-by-oom.sql
@@ -2,7 +2,7 @@ set preserve_insertion_order = false;
 
 copy (
   with
-    dates as (select unnest(generate_series(date '2010-01-01', date '2011-12-01', interval '1 day')) as days),
+    dates as (select unnest(generate_series(date '2010-01-01', date '2015-12-01', interval '1 day')) as days),
     ids   as (select unnest(generate_series(1, 100_000)) as id)
   from dates, ids
   select
