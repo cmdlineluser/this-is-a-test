fe25a9896fb5a9e1f8ea215c13e873db0d8588d2
Karl Genockey <99486669+cmdlineluser@users.noreply.github.com>
2025-11-26 13:07:18 +0000

    Refactor to read directly from URL and save as CSV

--- polars-sink-csv.py
+++ polars-sink-csv.py
@@ -4,25 +4,14 @@ from pathlib import Path
 
 url = "https://raw.githubusercontent.com/leanhdung1994/files/main/processedStep1_enwiktionary_namespace_0_43.ndjson"
 
-outNdjson = Path("wiktionary.ndjson")
-outTxt = Path("wiktionary.txt")
-
-# Download
-resp = requests.get(url)
-resp.raise_for_status()
-
-# Save
-with open(outNdjson, "wb") as f:
-    f.write(resp.content)
-
 # Read with Polars
-lf = pl.scan_ndjson(outNdjson)
-lf.select("html").sink_csv(outTxt,
+lf = pl.scan_ndjson(url)
+lf.select("html").sink_csv("1.csv",
                            include_header=False,
                            maintain_order=True,
                            quote_style="never",
                            line_terminator="\r\n")
 
-with open(outNdjson, "rb") as f:
+with open("1.csv", "rb") as f:
   for row in f:
     print(repr(row[-10:]))
